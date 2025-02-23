a7cd3e1d94df4386525b108661b997a54843faca
Evan Iwin Biju <evaniwinbiju@gmail.com>
2025-02-23 23:32:30 +0530

    added threading

--- src/main.zig
+++ src/main.zig
@@ -8,10 +8,10 @@ const http = std.http;
 const server_ip = "0.0.0.0";
 //change to port 80 on deployment
 const server_port: comptime_int = 8080;
-const index_html = @embedFile("index.html");
-const favicon = @embedFile("favicon.ico");
-const css = @embedFile("style.css");
-const script = @embedFile("script.js");
+const index_html = @embedFile("root/index.html");
+const favicon = @embedFile("root/favicon.ico");
+const css = @embedFile("root/style.css");
+const script = @embedFile("root/script.js");
 const REQUEST = enum {
     INDEX,
     CSS,
@@ -19,36 +19,47 @@ const REQUEST = enum {
     FAVICON,
     UNKNOWN,
 };
+var running = true;
+
+fn siginithandler() void {
+    running = false;
+}
+
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
     //parse address and returns an error if unable to parse given address
 
     const addr = net.Address.parseIp4(server_ip, server_port) catch |err| {
-        std.debug.print("An error occurred while resolving the IP address: {}\n", .{err});
+        std.log.err("An error occurred while resolving the IP address: {}\n", .{err});
         return;
     };
     //bind server to addr and listen
     var server = try addr.listen(.{});
-
-    start_server(&server);
+    defer server.deinit();
+    var pool: std.Thread.Pool = undefined;
+    try pool.init(.{ .allocator = allocator });
+    defer pool.deinit();
+    start_server(&server, &pool);
 }
 
-fn start_server(server: *net.Server) void {
-    while (true) {
+fn start_server(server: *net.Server, pool: *std.Thread.Pool) void {
+    //std.os.linux.sigaction(sig: u6, noalias act: ?*const Sigaction, noalias oact: ?*Sigaction)
+    while (running) {
         const connection = server.*.accept() catch |err| {
-            std.debug.print("Connection to client interrupted: {}\n", .{err});
+            std.log.err("Connection to client interrupted: {}\n", .{err});
             continue;
         };
-        const connectionthread = thread.spawn(.{}, handle_connection, .{connection}) catch |err| {
-            std.debug.print("Failed to spawn thread: {}\n", .{err});
+        _ = pool.*.spawn(handle_connection, .{connection}) catch |err| {
+            std.log.err("Failed to spawn thread: {}\n", .{err});
             continue;
         };
-        connectionthread.detach();
     }
 }
 
 fn loadfiles() !void {}
 
-fn parserequest(req: *http.Server.Request) REQUEST {
+fn ParseRequest(req: *http.Server.Request) REQUEST {
     if (std.mem.eql(u8, req.head.target, "/")) {
         return REQUEST.INDEX;
     } else if (std.mem.eql(u8, req.head.target, "/favicon.ico")) {
@@ -61,20 +72,8 @@ fn parserequest(req: *http.Server.Request) REQUEST {
         return REQUEST.UNKNOWN;
     }
 }
-
-fn handle_connection(connection: net.Server.Connection) !void {
-    defer connection.stream.close();
-
-    var read_buffer: [4096]u8 = undefined;
-    var http_server = http.Server.init(connection, &read_buffer);
-
-    var request = http_server.receiveHead() catch |err| {
-        std.debug.print("Could not read head: {}\n", .{err});
-        return;
-    };
-
-    std.debug.print("Handling request for {s}\n", .{request.head.target});
-    switch (parserequest(&request)) {
+fn respond(request: *http.Server.Request) !void {
+    switch (ParseRequest(request)) {
         REQUEST.INDEX => {
             try request.respond(index_html, .{});
         },
@@ -88,7 +87,23 @@ fn handle_connection(connection: net.Server.Connection) !void {
             try request.respond(favicon, .{});
         },
         REQUEST.UNKNOWN => {
-            std.debug.print("UNKNOWN REQUEST\n", .{});
+            std.log.info("UNKNOWN REQUEST\n", .{});
         },
     }
+}
+fn handle_connection(connection: net.Server.Connection) void {
+    defer connection.stream.close();
+
+    var read_buffer: [4096]u8 = undefined;
+    var http_server = http.Server.init(connection, &read_buffer);
+
+    var request = http_server.receiveHead() catch |err| {
+        std.debug.print("Could not read head: {}\n", .{err});
+        return;
+    };
+
+    std.log.info("Handling request for {s}\n", .{request.head.target});
+    respond(&request) catch |err| {
+        std.log.err("Unable to Respond: {}", .{err});
+    };
 }
