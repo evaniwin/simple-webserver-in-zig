994620ed677f222fdebba27cf5eabfda567b201f
Evan Iwin Biju <evaniwinbiju@gmail.com>
2025-02-26 16:21:31 +0530

    The server can now dynamicallly search the server root folder

--- src/main.zig
+++ src/main.zig
@@ -4,25 +4,43 @@ const fs = std.fs;
 const thread = std.Thread;
 const net = std.net;
 const http = std.http;
-
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const allocator = gpa.allocator();
 const server_ip = "0.0.0.0";
 //change to port 80 on deployment
-const server_port: comptime_int = 9080;
-const index_html = @embedFile("root/index.html");
-const favicon = @embedFile("root/favicon.ico");
-const css = @embedFile("root/style.css");
-const script = @embedFile("root/script.js");
-const icon = @embedFile("root/icon.jpeg");
+const server_port: comptime_int = 9090;
 var running = true;
 
 fn siginithandler() void {
     running = false;
 }
 
+const routing = struct {
+    path: []u8,
+    file: []u8,
+};
+
+const rootdir = "root";
+pub var rootdirfiles: std.ArrayList(routing) = undefined;
+
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
+
     //parse address and returns an error if unable to parse given address
+    rootdirfiles = std.ArrayList(routing).init(allocator);
+    defer rootdirfiles.deinit();
+
+    var cwd = std.fs.cwd();
+    var dir = cwd.openDir(rootdir, .{ .iterate = true }) catch |err| {
+        std.log.err("error {}", .{err});
+        return;
+    };
+    defer dir.close();
+    try dir.setAsCwd();
+    try traversedir(dir, "/");
+    defer freefiles();
+    for (rootdirfiles.items) |item| {
+        std.log.info("Path: {s}\n", .{item.path});
+    }
 
     const addr = net.Address.parseIp4(server_ip, server_port) catch |err| {
         std.log.err("An error occurred while resolving the IP address: {}\n", .{err});
@@ -37,6 +55,51 @@ pub fn main() !void {
     defer pool.deinit();
 
     start_server(&server, &pool);
+}
+fn traversedir(dir: std.fs.Dir, path: []const u8) !void {
+    var iter = dir.iterate();
+    while (true) {
+        const maybeEntry = try iter.next();
+        if (maybeEntry == null) break;
+        const entry = maybeEntry.?;
+        std.log.info("Found entry: {s}, {any}\n", .{ entry.name, entry.kind });
+
+        // If the entry is a file, open and read it:
+        if (entry.kind == .file) {
+            var file = dir.openFile(entry.name, .{}) catch |err| {
+                std.log.err("unable to open file {}", .{err});
+                continue;
+            };
+            defer file.close();
+
+            const buf = file.readToEndAlloc(allocator, 1048576) catch |err| {
+                std.log.err("unable to read file {}", .{err});
+                continue;
+            };
+            const pathrel = try std.fmt.allocPrint(allocator, "{s}{s}", .{ path, entry.name });
+            const temp = routing{
+                .path = pathrel,
+                .file = buf,
+            };
+            try rootdirfiles.append(temp);
+            //std.log.info("Content: {s}\n", .{buf});
+        } else if (entry.kind == std.fs.File.Kind.directory) {
+            var subdir = dir.openDir(entry.name, .{ .iterate = true }) catch |err| {
+                std.log.err("error {}", .{err});
+                return;
+            };
+            defer subdir.close();
+            const pathrel = try std.fmt.allocPrint(allocator, "{s}{s}/", .{ path, entry.name });
+
+            try traversedir(subdir, pathrel);
+        }
+    }
+}
+
+fn freefiles() void {
+    for (rootdirfiles.items) |val| {
+        allocator.free(val.file);
+    }
 }
 
 fn start_server(server: *net.Server, pool: *std.Thread.Pool) void {
@@ -54,23 +117,18 @@ fn start_server(server: *net.Server, pool: *std.Thread.Pool) void {
     }
 }
 
-fn loadfiles() !void {}
-
 fn respond(request: *http.Server.Request) !void {
     if (request.head.method == http.Method.GET) {
-        if (std.mem.eql(u8, request.head.target, "/")) {
-            try request.respond(index_html, .{});
-        } else if (std.mem.eql(u8, request.head.target, "/favicon.ico")) {
-            try request.respond(favicon, .{});
-        } else if (std.mem.eql(u8, request.head.target, "/style.css")) {
-            try request.respond(css, .{});
-        } else if (std.mem.eql(u8, request.head.target, "/script.js")) {
-            try request.respond(script, .{});
-        } else if (std.mem.eql(u8, request.head.target, "/icon.jpeg")) {
-            try request.respond(icon, .{});
-        } else {
-            try request.respond("404 NOT FOUND", .{});
+        for (rootdirfiles.items) |value| {
+            if (std.mem.eql(u8, request.head.target, "/") and std.mem.eql(u8, value.path, "/index.html")) {
+                try request.respond(value.file, .{});
+                return;
+            } else if (std.mem.eql(u8, request.head.target, value.path)) {
+                try request.respond(value.file, .{});
+                return;
+            }
         }
+        try request.respond("404 NOT FOUND", .{});
     }
 }
 fn handle_connection(connection: net.Server.Connection) void {
